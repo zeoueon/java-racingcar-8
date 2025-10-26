10f2f5747a53bd68bb16c0567d28e75ae59b4649
zeoueon <zeoueon129@gmail.com>
2025-10-27 01:46:24 +0900

    feat(ErrorMessage): 에러메시지 상수 선언
    상황 별 에러메시지를 상수로 선언한다.

---
+++ src/main/java/racingcar/exception/ErrorMessage.java
@@ -0,0 +1,9 @@
+package racingcar.exception;
+
+public class ErrorMessage {
+
+    public static final String NOT_INTEGER_TYPE = "정수로 입력해야 합니다.";
+    public static final String INVALID_ATTEMPT_COUNT = "시도 횟수는 1 이상인 정수여야 합니다.";
+    public static final String INVALID_CAR_NAME_LENGTH = "자동차 이름은 5자 이내여야 합니다.";
+    public static final String NOT_DUPLICATE_CAR_NAMES = "자동차 이름을 중복없이 입력해야 합니다.";
+}
