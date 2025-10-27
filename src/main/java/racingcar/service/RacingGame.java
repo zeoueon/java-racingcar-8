e601792468cbe761e168c52fc811b4247c49e862
zeoueon <zeoueon129@gmail.com>
2025-10-27 12:53:43 +0900

    feat(RacingGame): 하나의 레이싱 게임을 운영하는 객체 구현
    시도할 횟수만큼 레이싱 게임을 진행한다.
    매 라운드의 스냅샷을 RoundResultDto를 생성해 저장한다.
    우승자를 문자열 리스트 형태로 반환한다.

---
+++ src/main/java/racingcar/service/RacingGame.java
@@ -0,0 +1,46 @@
+package racingcar.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+import racingcar.dto.RoundResultDto;
+
+public class RacingGame {
+
+    private Cars cars;
+    private int attemptCount;
+    private List<RoundResultDto> roundResults = new ArrayList<>();
+
+    public RacingGame(List<String> carNames, int attemptCount) {
+        this.cars = new Cars(carNames);
+        this.attemptCount = attemptCount;
+    }
+
+    public void start() {
+        while (attemptCount > 0) {
+            cars.moveAndStop();
+            roundResults.add(new RoundResultDto(getSnapshot()));
+
+            attemptCount--;
+        }
+    }
+
+    private Map<String, Integer> getSnapshot() {
+        List<Car> currentCars = cars.getCurrentStateOfCars();
+        return currentCars.stream()
+                .collect(Collectors.toMap(Car::getName, Car::getMoveCount));
+    }
+
+    public List<RoundResultDto> getRoundResults() {
+        return roundResults;
+    }
+
+    public List<String> getWinners() {
+        List<Car> winners = cars.findWinner();
+        return winners.stream().map(Car::getName)
+                .collect(Collectors.toList());
+    }
+}
