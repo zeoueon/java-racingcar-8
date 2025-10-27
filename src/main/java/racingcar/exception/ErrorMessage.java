9bf3258e2e780c9776471d45e3e35fd6d93873f9
zeoueon <zeoueon129@gmail.com>
2025-10-27 21:55:36 +0900

    feat(ErrorMessage): 자동차 개수 예외 문자
    자동차 최소 개수 예외 문자를 상수로 선언한다.

--- src/main/java/racingcar/exception/ErrorMessage.java
+++ src/main/java/racingcar/exception/ErrorMessage.java
@@ -6,4 +6,5 @@ public class ErrorMessage {
     public static final String INVALID_ATTEMPT_COUNT = "시도 횟수는 1 이상인 정수여야 합니다.";
     public static final String INVALID_CAR_NAME_LENGTH = "자동차 이름은 5자 이내여야 합니다.";
     public static final String NOT_DUPLICATE_CAR_NAMES = "자동차 이름을 중복없이 입력해야 합니다.";
+    public static final String MINIMUM_CAR_COUNT = "자동차는 2대 이상 입력해야 합니다";
 }
