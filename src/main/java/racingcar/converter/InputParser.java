e5daa1279654452460c2f88c1301bde7a00c355f
zeoueon <zeoueon129@gmail.com>
2025-10-27 13:42:08 +0900

    fix(InputParser): 공백 제거 안되는 문법 오류 수정
    공백 제거가 안되는 문제를 해결한다.

--- src/main/java/racingcar/converter/InputParser.java
+++ src/main/java/racingcar/converter/InputParser.java
@@ -6,13 +6,9 @@ import java.util.List;
 public class InputParser {
 
     public List<String> parseCarNamesToList(String carNames) {
-        List<String> carNamesList = Arrays.asList(carNames.split(","));
-
-        for (String carName : carNamesList) {
-            carName = carName.trim();
-        }
-
-        return carNamesList;
+        return Arrays.stream(carNames.split(","))
+                .map(String::trim)
+                .toList();
     }
 
     public int parseAttemptCountToInt(String attemptCount) {
